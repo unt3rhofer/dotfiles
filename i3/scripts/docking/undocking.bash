6c723f0305116a0f286928cc4ea0e92a8552f16b
unt3rhofer <martin.unter@icloud.com>
2024-10-21 08:06:13 -0400

    Newer scripts for docking and undocking
    (undocking doesen't really work as good, will change when fixed

---
+++ i3/scripts/docking/undocking.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Name of the external display and laptop display
+EXTERNAL_OUTPUT="HDMI-1"
+INTERNAL_OUTPUT="eDP-1"
+
+# Function to check if the external display is disconnected
+check_external_display_disconnected() {
+    xrandr | grep "$EXTERNAL_OUTPUT disconnected"
+}
+
+# Main loop to monitor display status
+while true; do
+    if check_external_display_disconnected; then
+        # If the external display is disconnected, turn the internal display back on
+        xrandr --output "$INTERNAL_OUTPUT" --auto
+        break
+    fi
+    # Sleep for 5 seconds before checking again
+    sleep 5
+done
+

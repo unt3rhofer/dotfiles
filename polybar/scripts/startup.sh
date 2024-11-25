b0b6b0d5f35b1d781a3b25858d53831fe04b9324
unt3rhofer <martin.unter@icloud.com>
2024-11-25 23:36:40 +0100

    2024-11-25@23:36

---
+++ polybar/scripts/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar minimal 2>&1 | tee -a /tmp/polybar.log & disown
+
